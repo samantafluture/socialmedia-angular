6e94b302d98a5bec35d4bb2628f5dcd3579622ae
Samanta Fluture <samantafluture@gmail.com>
2021-08-28 22:21:31 -0300

    create custom validatons

--- src/app/home/novo-usuario/novo-usuario.component.ts
+++ src/app/home/novo-usuario/novo-usuario.component.ts
@@ -1,15 +1,33 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { minusculoValidator } from './minusculo.validator';
+import { NovoUsuario } from './novo-usuario';
+import { NovoUsuarioService } from './novo-usuario.service';
 
 @Component({
   selector: 'app-novo-usuario',
   templateUrl: './novo-usuario.component.html',
-  styleUrls: ['./novo-usuario.component.css']
+  styleUrls: ['./novo-usuario.component.css'],
 })
 export class NovoUsuarioComponent implements OnInit {
+  novoUsuarioForm!: FormGroup;
 
-  constructor() { }
+  constructor(
+    private formBuilder: FormBuilder,
+    private novoUsuarioService: NovoUsuarioService
+  ) {}
 
   ngOnInit(): void {
+    this.novoUsuarioForm = this.formBuilder.group({
+      email: ['', [Validators.required, Validators.email]],
+      fullName: ['', [Validators.required, Validators.minLength(4)]],
+      userName: ['', [minusculoValidator]],
+      password: [''],
+    });
   }
 
+  cadastrar() {
+    const novoUsario = this.novoUsuarioForm.getRawValue() as NovoUsuario;
+    console.log(novoUsario);
+  }
 }

355d232781a41bb928f15ea1a16d124fb05862c6
Samanta Fluture <samantafluture@gmail.com>
2021-08-29 12:00:40 -0300

    create footer and header components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,21 @@
+import { RodapeModule } from './componentes/rodape/rodape.module';
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CabecalhoModule } from './componentes/cabecalho/cabecalho.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, HttpClientModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    CabecalhoModule,
+    RodapeModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
